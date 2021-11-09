14d11608b04b18cceafe4fd79d815540924d34f0
Salahuddin <salahuddinace56@gmail.com>
2021-11-09 20:58:23 +0600

    First Commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int j = 5;
+            j = 10;
+            Console.WriteLine("Hello World!");
+            string name = Console.ReadLine();
+            
+            string name2 = Console.ReadLine();
+            Console.WriteLine("Welcome {1},{0}", name, name2);
+            Console.WriteLine("Welcome "+name+" ,"+name2);
+            Console.ReadLine();
+
+        }
+    }
+}
